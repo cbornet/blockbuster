e834b7d1ce06d84e40f8ae840a0b165b41975691
Christophe Bornet <cbornet@hotmail.com>
2024-11-21 19:18:13 +0100

    Add blockbuster and tests

---
+++ blockbuster/blockbuster.py
@@ -0,0 +1,193 @@
+"""BlockBuster module."""
+
+import asyncio
+import inspect
+import io
+import os
+import socket
+import ssl
+import sys
+import time
+from contextlib import contextmanager
+
+import forbiddenfruit
+
+
+class BlockingError(Exception):
+    """BlockingError class."""
+
+
+def _blocking_error(func):
+    if inspect.isbuiltin(func):
+        msg = f"Blocking call to {func.__qualname__} ({func.__self__})"
+    elif inspect.ismethoddescriptor(func):
+        msg = f"Blocking call to {func}"
+    else:
+        msg = f"Blocking call to {func.__module__}.{func.__qualname__}"
+    return BlockingError(msg)
+
+
+def wrap_blocking(func, stack_excludes, func_excludes):
+    """Wrap blocking function."""
+
+    def wrapper(*args, **kwargs):
+        try:
+            asyncio.get_running_loop()
+        except RuntimeError:
+            return func(*args, **kwargs)
+        for filename, functions in stack_excludes:
+            for frame_info in inspect.stack():
+                if (
+                    frame_info.filename.endswith(filename)
+                    and frame_info.function in functions
+                ):
+                    return func(*args, **kwargs)
+        for func_exclude in func_excludes:
+            if func_exclude(*args, **kwargs):
+                return func(*args, **kwargs)
+        raise _blocking_error(func)
+
+    return wrapper
+
+
+class BlockBusterFunction:
+    """BlockBusterFunction class."""
+
+    def __init__(
+        self,
+        module,
+        func_name: str,
+        *,
+        is_immutable=False,
+        stack_excludes=None,
+        func_excludes=None,
+        checker_func=wrap_blocking,
+    ):
+        """Initialize BlockBusterFunction."""
+        self.module = module
+        self.func_name = func_name
+        self.original_func = getattr(module, func_name)
+        self.is_immutable = is_immutable
+        self.stack_excludes = stack_excludes or []
+        self.func_excludes = func_excludes or []
+        self.checker_func = checker_func
+
+    def wrap_blocking(self):
+        """Wrap the function."""
+        checker = self.checker_func(
+            self.original_func, self.stack_excludes, self.func_excludes
+        )
+        if self.is_immutable:
+            forbiddenfruit.curse(self.module, self.func_name, checker)
+        else:
+            setattr(self.module, self.func_name, checker)
+
+    def unwrap_blocking(self):
+        """Unwrap the function."""
+        if self.is_immutable:
+            forbiddenfruit.curse(self.module, self.func_name, self.original_func)
+        else:
+            setattr(self.module, self.func_name, self.original_func)
+
+
+class BlockBuster:
+    """BlockBuster class."""
+
+    def __init__(self):
+        """Initialize BlockBuster."""
+
+        def os_exclude(fd, *_, **__):
+            return not os.get_blocking(fd)
+
+        def socket_exclude(sock, *_, **__):
+            return not sock.getblocking()
+
+        def file_write_exclude(file, *_, **__):
+            return file in {sys.stdout, sys.stderr}
+
+        self.wrapped_functions = {
+            "time.sleep": BlockBusterFunction(
+                time,
+                "sleep",
+                stack_excludes=[("pydev/pydevd.py", {"_do_wait_suspend"})],
+            ),
+            "io.BufferedReader.read": BlockBusterFunction(
+                io.BufferedReader,
+                "read",
+                is_immutable=True,
+                stack_excludes=[
+                    ("<frozen importlib._bootstrap_external>", {"get_data"}),
+                    ("_pytest/assertion/rewrite.py", {"_rewrite_test", "_read_pyc"}),
+                ],
+            ),
+            "io.BufferedWriter.write": BlockBusterFunction(
+                io.BufferedWriter,
+                "write",
+                is_immutable=True,
+                stack_excludes=[("_pytest/assertion/rewrite.py", {"_write_pyc"})],
+                func_excludes=[file_write_exclude],
+            ),
+            "io.BufferedRandom.read": BlockBusterFunction(
+                io.BufferedRandom, "read", is_immutable=True
+            ),
+            "io.BufferedRandom.write": BlockBusterFunction(
+                io.BufferedRandom,
+                "write",
+                is_immutable=True,
+                func_excludes=[file_write_exclude],
+            ),
+            "io.TextIOWrapper.read": BlockBusterFunction(
+                io.TextIOWrapper, "read", is_immutable=True
+            ),
+            "io.TextIOWrapper.write": BlockBusterFunction(
+                io.TextIOWrapper,
+                "write",
+                is_immutable=True,
+                func_excludes=[file_write_exclude],
+            ),
+        }
+
+        for method in ("read", "write"):
+            self.wrapped_functions[f"os.{method}"] = BlockBusterFunction(
+                os, method, func_excludes=[os_exclude]
+            )
+
+        for method in (
+            "connect",
+            "accept",
+            "send",
+            "sendall",
+            "sendto",
+            "recv",
+            "recv_into",
+            "recvfrom",
+            "recvfrom_into",
+            "recvmsg",
+        ):
+            self.wrapped_functions[f"socket.socket.{method}"] = BlockBusterFunction(
+                socket.socket, method, func_excludes=[socket_exclude]
+            )
+
+        for method in ("write", "send", "read", "recv"):
+            self.wrapped_functions[f"ssl.SSLSocket.{method}"] = BlockBusterFunction(
+                ssl.SSLSocket, method, func_excludes=[socket_exclude]
+            )
+
+    def init(self):
+        """Wrap all functions."""
+        for wrapped_function in self.wrapped_functions.values():
+            wrapped_function.wrap_blocking()
+
+    def cleanup(self):
+        """Unwrap all wrapped functions."""
+        for wrapped_function in self.wrapped_functions.values():
+            wrapped_function.unwrap_blocking()
+
+
+@contextmanager
+def blockbuster_ctx():
+    """Context manager for using BlockBuster."""
+    blockbuster = BlockBuster()
+    blockbuster.init()
+    yield blockbuster
+    blockbuster.cleanup()
---
+++ tests/test_blockbuster.py
@@ -0,0 +1,178 @@
+import asyncio
+import importlib
+import io
+import os
+import re
+import socket
+import threading
+import time
+from pathlib import Path
+
+import pytest
+import requests
+
+from blockbuster import BlockingError, blockbuster_ctx
+
+
+@pytest.fixture(autouse=True)
+def blockbuster():
+    with blockbuster_ctx() as bb:
+        yield bb
+
+
+async def test_time_sleep():
+    with pytest.raises(
+        BlockingError, match=re.escape("sleep (<module 'time' (built-in)>")
+    ):
+        time.sleep(1)  # noqa: ASYNC251
+
+
+async def test_os_read():
+    fd = os.open("/dev/null", os.O_RDONLY)
+    with pytest.raises(
+        BlockingError, match=re.escape("read (<module 'posix' (built-in)>")
+    ):
+        os.read(fd, 1)
+
+
+async def test_os_read_non_blocking():
+    fd = os.open("/dev/null", os.O_NONBLOCK | os.O_RDONLY)
+    os.read(fd, 1)
+
+
+async def test_os_write():
+    fd = os.open("/dev/null", os.O_RDWR)
+    with pytest.raises(
+        BlockingError, match=re.escape("write (<module 'posix' (built-in)>")
+    ):
+        os.write(fd, b"foo")
+
+
+async def test_os_write_non_blocking():
+    fd = os.open("/dev/null", os.O_NONBLOCK | os.O_RDWR)
+    os.write(fd, b"foo")
+
+
+PORT = 65432
+
+
+def tcp_server():
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.bind(("127.0.0.1", PORT))
+        s.listen()
+        conn, addr = s.accept()
+        with conn:
+            while True:
+                conn.sendall(b"Hello, world")
+                data = conn.recv(1024)
+                if not data:
+                    break
+                conn.sendall(data)
+
+
+async def test_socket_connect():
+    with (
+        socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s,
+        pytest.raises(BlockingError, match="method 'connect' of '_socket.socket'"),
+    ):
+        s.connect(("127.0.0.1", PORT))
+
+
+async def test_socket_send():
+    t = threading.Thread(target=tcp_server)
+    t.start()
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        while True:
+            try:
+                await asyncio.to_thread(s.connect, ("127.0.0.1", PORT))
+                break
+            except ConnectionRefusedError:
+                await asyncio.sleep(0.1)
+        with pytest.raises(BlockingError, match="method 'send' of '_socket.socket'"):
+            s.send(b"Hello, world")
+    await asyncio.to_thread(t.join)
+
+
+async def test_socket_send_non_blocking():
+    t = threading.Thread(target=tcp_server)
+    t.start()
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        while True:
+            try:
+                await asyncio.to_thread(s.connect, ("127.0.0.1", PORT))
+                break
+            except ConnectionRefusedError:
+                await asyncio.sleep(0.1)
+        s.setblocking(False)  # noqa: FBT003
+        s.send(b"Hello, world")
+    await asyncio.to_thread(t.join)
+
+
+async def test_ssl_socket(blockbuster):
+    blockbuster.wrapped_functions["socket.socket.connect"].unwrap_blocking()
+    with pytest.raises(BlockingError, match="ssl.SSLSocket.send"):
+        requests.get("https://google.com", timeout=10)  # noqa: ASYNC210
+
+
+async def test_file_text():
+    with Path("/dev/null").open(mode="r+") as f:  # noqa: ASYNC230
+        assert isinstance(f, io.TextIOWrapper)
+        with pytest.raises(
+            BlockingError, match="method 'write' of '_io.TextIOWrapper'"
+        ):
+            f.write("foo")
+        with pytest.raises(BlockingError, match="method 'read' of '_io.TextIOWrapper'"):
+            f.read(1)
+
+
+async def test_file_random():
+    with Path("/dev/null").open(mode="r+b") as f:  # noqa: ASYNC230
+        assert isinstance(f, io.BufferedRandom)
+        with pytest.raises(
+            BlockingError, match="method 'write' of '_io.BufferedRandom'"
+        ):
+            f.write(b"foo")
+        with pytest.raises(
+            BlockingError, match="method 'read' of '_io.BufferedRandom'"
+        ):
+            f.read(1)
+
+
+async def test_file_read_bytes():
+    with Path("/dev/null").open(mode="rb") as f:  # noqa: ASYNC230
+        assert isinstance(f, io.BufferedReader)
+        with pytest.raises(
+            BlockingError, match="method 'read' of '_io.BufferedReader'"
+        ):
+            f.read(1)
+
+
+async def test_file_write_bytes():
+    with Path("/dev/null").open(mode="wb") as f:  # noqa: ASYNC230
+        assert isinstance(f, io.BufferedWriter)
+        with pytest.raises(
+            BlockingError, match="method 'write' of '_io.BufferedWriter'"
+        ):
+            f.write(b"foo")
+
+
+async def test_import_module_exclude():
+    importlib.reload(requests)
+
+
+def allowed_read():
+    with Path("/dev/null").open(mode="rb") as f:
+        f.read(1)
+
+
+async def test_custom_stack_exclude(blockbuster):
+    blockbuster.wrapped_functions["io.BufferedReader.read"].stack_excludes.append(
+        ("tests/test_blockbuster.py", {"allowed_read"})
+    )
+    allowed_read()
+
+
+async def test_cleanup(blockbuster):
+    blockbuster.cleanup()
+    with Path("/dev/null").open(mode="wb") as f:  # noqa: ASYNC230
+        f.write(b"foo")
