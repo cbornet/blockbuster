ec6cabd0282b3c266f999772cd7d16ac31431f0c
Christophe Bornet <cbornet@hotmail.com>
2024-12-21 21:50:37 +0100

    Add README (#25)

--- blockbuster/__init__.py
+++ blockbuster/__init__.py
@@ -1,5 +1,10 @@
 """Blockbuster is a utility to detect blocking calls in the async event loop."""
 
-from blockbuster.blockbuster import BlockBuster, BlockingError, blockbuster_ctx
+from blockbuster.blockbuster import (
+    BlockBuster,
+    BlockBusterFunction,
+    BlockingError,
+    blockbuster_ctx,
+)
 
-__all__ = ["BlockBuster", "BlockingError", "blockbuster_ctx"]
+__all__ = ["BlockBuster", "BlockBusterFunction", "BlockingError", "blockbuster_ctx"]
